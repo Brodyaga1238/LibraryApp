d56ca1a5bdbdef820f8e2cbc61752e8a1a61b21b
Brodyaga1238 <nurlanhasanov1238@gmail.com>
2023-06-10 18:55:40 +0300

    Initial commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace LibraryDesktopApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.tbBookTitle = new System.Windows.Forms.TextBox();
+            this.tbBookAuthor = new System.Windows.Forms.TextBox();
+            this.lblBookTitle = new System.Windows.Forms.Label();
+            this.lblBookAuthor = new System.Windows.Forms.Label();
+            this.lbBooks = new System.Windows.Forms.ListBox();
+            this.lblBooks = new System.Windows.Forms.Label();
+            this.btnAddReader = new System.Windows.Forms.Button();
+            this.tbReaderName = new System.Windows.Forms.TextBox();
+            this.lblReaderName = new System.Windows.Forms.Label();
+            this.lbReaders = new System.Windows.Forms.ListBox();
+            this.lblReaders = new System.Windows.Forms.Label();
+            this.btnIssueBook = new System.Windows.Forms.Button();
+            this.lbIssuedBooks = new System.Windows.Forms.ListBox();
+            this.lblIssuedBooks = new System.Windows.Forms.Label();
+            this.btnReturnBook = new System.Windows.Forms.Button();
+            this.lbReturnedBooks = new System.Windows.Forms.ListBox();
+            this.lblReturnedBooks = new System.Windows.Forms.Label();
+            this.btnGetAllBooks = new System.Windows.Forms.Button();
+            this.btnGetAllReaders = new System.Windows.Forms.Button();
+            this.btnGetIssuedBooks = new System.Windows.Forms.Button();
+            this.btnGetReturnedBooks = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.BackColor = System.Drawing.Color.Beige;
+            this.btnAddBook.Location = new System.Drawing.Point(12, 12);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(120, 23);
+            this.btnAddBook.TabIndex = 21;
+            this.btnAddBook.Text = "Добавить книгу";
+            this.btnAddBook.UseVisualStyleBackColor = false;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
+            // 
+            // tbBookTitle
+            // 
+            this.tbBookTitle.Location = new System.Drawing.Point(138, 14);
+            this.tbBookTitle.Name = "tbBookTitle";
+            this.tbBookTitle.Size = new System.Drawing.Size(150, 20);
+            this.tbBookTitle.TabIndex = 20;
+            // 
+            // tbBookAuthor
+            // 
+            this.tbBookAuthor.Location = new System.Drawing.Point(138, 40);
+            this.tbBookAuthor.Name = "tbBookAuthor";
+            this.tbBookAuthor.Size = new System.Drawing.Size(150, 20);
+            this.tbBookAuthor.TabIndex = 19;
+            // 
+            // lblBookTitle
+            // 
+            this.lblBookTitle.AutoSize = true;
+            this.lblBookTitle.Location = new System.Drawing.Point(294, 17);
+            this.lblBookTitle.Name = "lblBookTitle";
+            this.lblBookTitle.Size = new System.Drawing.Size(57, 13);
+            this.lblBookTitle.TabIndex = 18;
+            this.lblBookTitle.Text = "Название";
+            // 
+            // lblBookAuthor
+            // 
+            this.lblBookAuthor.AutoSize = true;
+            this.lblBookAuthor.Location = new System.Drawing.Point(294, 43);
+            this.lblBookAuthor.Name = "lblBookAuthor";
+            this.lblBookAuthor.Size = new System.Drawing.Size(37, 13);
+            this.lblBookAuthor.TabIndex = 17;
+            this.lblBookAuthor.Text = "Автор";
+            // 
+            // lbBooks
+            // 
+            this.lbBooks.FormattingEnabled = true;
+            this.lbBooks.Location = new System.Drawing.Point(12, 87);
+            this.lbBooks.Name = "lbBooks";
+            this.lbBooks.Size = new System.Drawing.Size(360, 95);
+            this.lbBooks.TabIndex = 16;
+            // 
+            // lblBooks
+            // 
+            this.lblBooks.AutoSize = true;
+            this.lblBooks.Location = new System.Drawing.Point(12, 71);
+            this.lblBooks.Name = "lblBooks";
+            this.lblBooks.Size = new System.Drawing.Size(37, 13);
+            this.lblBooks.TabIndex = 15;
+            this.lblBooks.Text = "Книги";
+            // 
+            // btnAddReader
+            // 
+            this.btnAddReader.BackColor = System.Drawing.Color.Beige;
+            this.btnAddReader.Location = new System.Drawing.Point(357, 17);
+            this.btnAddReader.Name = "btnAddReader";
+            this.btnAddReader.Size = new System.Drawing.Size(150, 23);
+            this.btnAddReader.TabIndex = 14;
+            this.btnAddReader.Text = "Добавить читателя";
+            this.btnAddReader.UseVisualStyleBackColor = false;
+            this.btnAddReader.Click += new System.EventHandler(this.btnAddReader_Click);
+            // 
+            // tbReaderName
+            // 
+            this.tbReaderName.Location = new System.Drawing.Point(513, 20);
+            this.tbReaderName.Name = "tbReaderName";
+            this.tbReaderName.Size = new System.Drawing.Size(150, 20);
+            this.tbReaderName.TabIndex = 13;
+            // 
+            // lblReaderName
+            // 
+            this.lblReaderName.AutoSize = true;
+            this.lblReaderName.Location = new System.Drawing.Point(669, 27);
+            this.lblReaderName.Name = "lblReaderName";
+            this.lblReaderName.Size = new System.Drawing.Size(29, 13);
+            this.lblReaderName.TabIndex = 12;
+            this.lblReaderName.Text = "Имя";
+            // 
+            // lbReaders
+            // 
+            this.lbReaders.FormattingEnabled = true;
+            this.lbReaders.Location = new System.Drawing.Point(378, 87);
+            this.lbReaders.Name = "lbReaders";
+            this.lbReaders.Size = new System.Drawing.Size(360, 95);
+            this.lbReaders.TabIndex = 11;
+            // 
+            // lblReaders
+            // 
+            this.lblReaders.AutoSize = true;
+            this.lblReaders.Location = new System.Drawing.Point(357, 71);
+            this.lblReaders.Name = "lblReaders";
+            this.lblReaders.Size = new System.Drawing.Size(55, 13);
+            this.lblReaders.TabIndex = 10;
+            this.lblReaders.Text = "Читатели";
+            // 
+            // btnIssueBook
+            // 
+            this.btnIssueBook.BackColor = System.Drawing.Color.Beige;
+            this.btnIssueBook.Location = new System.Drawing.Point(12, 188);
+            this.btnIssueBook.Name = "btnIssueBook";
+            this.btnIssueBook.Size = new System.Drawing.Size(150, 23);
+            this.btnIssueBook.TabIndex = 9;
+            this.btnIssueBook.Text = "Выдать книгу";
+            this.btnIssueBook.UseVisualStyleBackColor = false;
+            this.btnIssueBook.Click += new System.EventHandler(this.btnIssueBook_Click);
+            // 
+            // lbIssuedBooks
+            // 
+            this.lbIssuedBooks.FormattingEnabled = true;
+            this.lbIssuedBooks.Location = new System.Drawing.Point(12, 241);
+            this.lbIssuedBooks.Name = "lbIssuedBooks";
+            this.lbIssuedBooks.Size = new System.Drawing.Size(360, 95);
+            this.lbIssuedBooks.TabIndex = 8;
+            // 
+            // lblIssuedBooks
+            // 
+            this.lblIssuedBooks.AutoSize = true;
+            this.lblIssuedBooks.Location = new System.Drawing.Point(12, 225);
+            this.lblIssuedBooks.Name = "lblIssuedBooks";
+            this.lblIssuedBooks.Size = new System.Drawing.Size(92, 13);
+            this.lblIssuedBooks.TabIndex = 7;
+            this.lblIssuedBooks.Text = "Выданные книги";
+            // 
+            // btnReturnBook
+            // 
+            this.btnReturnBook.BackColor = System.Drawing.Color.Beige;
+            this.btnReturnBook.Location = new System.Drawing.Point(378, 188);
+            this.btnReturnBook.Name = "btnReturnBook";
+            this.btnReturnBook.Size = new System.Drawing.Size(150, 23);
+            this.btnReturnBook.TabIndex = 6;
+            this.btnReturnBook.Text = "Вернуть книгу";
+            this.btnReturnBook.UseVisualStyleBackColor = false;
+            this.btnReturnBook.Click += new System.EventHandler(this.btnReturnBook_Click);
+            // 
+            // lbReturnedBooks
+            // 
+            this.lbReturnedBooks.FormattingEnabled = true;
+            this.lbReturnedBooks.Location = new System.Drawing.Point(378, 241);
+            this.lbReturnedBooks.Name = "lbReturnedBooks";
+            this.lbReturnedBooks.Size = new System.Drawing.Size(360, 95);
+            this.lbReturnedBooks.TabIndex = 5;
+            // 
+            // lblReturnedBooks
+            // 
+            this.lblReturnedBooks.AutoSize = true;
+            this.lblReturnedBooks.Location = new System.Drawing.Point(378, 225);
+            this.lblReturnedBooks.Name = "lblReturnedBooks";
+            this.lblReturnedBooks.Size = new System.Drawing.Size(117, 13);
+            this.lblReturnedBooks.TabIndex = 4;
+            this.lblReturnedBooks.Text = "Возвращенные книги";
+            // 
+            // btnGetAllBooks
+            // 
+            this.btnGetAllBooks.BackColor = System.Drawing.Color.Beige;
+            this.btnGetAllBooks.Location = new System.Drawing.Point(12, 342);
+            this.btnGetAllBooks.Name = "btnGetAllBooks";
+            this.btnGetAllBooks.Size = new System.Drawing.Size(306, 23);
+            this.btnGetAllBooks.TabIndex = 3;
+            this.btnGetAllBooks.Text = "Получить все книги";
+            this.btnGetAllBooks.UseVisualStyleBackColor = false;
+            this.btnGetAllBooks.Click += new System.EventHandler(this.btnGetAllBooks_Click);
+            // 
+            // btnGetAllReaders
+            // 
+            this.btnGetAllReaders.BackColor = System.Drawing.Color.Beige;
+            this.btnGetAllReaders.Location = new System.Drawing.Point(12, 371);
+            this.btnGetAllReaders.Name = "btnGetAllReaders";
+            this.btnGetAllReaders.Size = new System.Drawing.Size(306, 23);
+            this.btnGetAllReaders.TabIndex = 2;
+            this.btnGetAllReaders.Text = "Получить всех читателей";
+            this.btnGetAllReaders.UseVisualStyleBackColor = false;
+            this.btnGetAllReaders.Click += new System.EventHandler(this.btnGetAllReaders_Click);
+            // 
+            // btnGetIssuedBooks
+            // 
+            this.btnGetIssuedBooks.BackColor = System.Drawing.Color.Beige;
+            this.btnGetIssuedBooks.Location = new System.Drawing.Point(378, 342);
+            this.btnGetIssuedBooks.Name = "btnGetIssuedBooks";
+            this.btnGetIssuedBooks.Size = new System.Drawing.Size(306, 23);
+            this.btnGetIssuedBooks.TabIndex = 1;
+            this.btnGetIssuedBooks.Text = "Получить выданные книги";
+            this.btnGetIssuedBooks.UseVisualStyleBackColor = false;
+            this.btnGetIssuedBooks.Click += new System.EventHandler(this.btnGetIssuedBooks_Click);
+            // 
+            // btnGetReturnedBooks
+            // 
+            this.btnGetReturnedBooks.BackColor = System.Drawing.Color.Beige;
+            this.btnGetReturnedBooks.Location = new System.Drawing.Point(378, 371);
+            this.btnGetReturnedBooks.Name = "btnGetReturnedBooks";
+            this.btnGetReturnedBooks.Size = new System.Drawing.Size(306, 23);
+            this.btnGetReturnedBooks.TabIndex = 0;
+            this.btnGetReturnedBooks.Text = "Получить возвращенные книги";
+            this.btnGetReturnedBooks.UseVisualStyleBackColor = false;
+            this.btnGetReturnedBooks.Click += new System.EventHandler(this.btnGetReturnedBooks_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(740, 629);
+            this.Controls.Add(this.btnGetReturnedBooks);
+            this.Controls.Add(this.btnGetIssuedBooks);
+            this.Controls.Add(this.btnGetAllReaders);
+            this.Controls.Add(this.btnGetAllBooks);
+            this.Controls.Add(this.lblReturnedBooks);
+            this.Controls.Add(this.lbReturnedBooks);
+            this.Controls.Add(this.btnReturnBook);
+            this.Controls.Add(this.lblIssuedBooks);
+            this.Controls.Add(this.lbIssuedBooks);
+            this.Controls.Add(this.btnIssueBook);
+            this.Controls.Add(this.lblReaders);
+            this.Controls.Add(this.lbReaders);
+            this.Controls.Add(this.lblReaderName);
+            this.Controls.Add(this.tbReaderName);
+            this.Controls.Add(this.btnAddReader);
+            this.Controls.Add(this.lblBooks);
+            this.Controls.Add(this.lbBooks);
+            this.Controls.Add(this.lblBookAuthor);
+            this.Controls.Add(this.lblBookTitle);
+            this.Controls.Add(this.tbBookAuthor);
+            this.Controls.Add(this.tbBookTitle);
+            this.Controls.Add(this.btnAddBook);
+            this.Name = "Form1";
+            this.Text = "Library Desktop App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.TextBox tbBookTitle;
+        private System.Windows.Forms.TextBox tbBookAuthor;
+        private System.Windows.Forms.Label lblBookTitle;
+        private System.Windows.Forms.Label lblBookAuthor;
+        private System.Windows.Forms.ListBox lbBooks;
+        private System.Windows.Forms.Label lblBooks;
+        private System.Windows.Forms.Button btnAddReader;
+        private System.Windows.Forms.TextBox tbReaderName;
+        private System.Windows.Forms.Label lblReaderName;
+        private System.Windows.Forms.ListBox lbReaders;
+        private System.Windows.Forms.Label lblReaders;
+        private System.Windows.Forms.Button btnIssueBook;
+        private System.Windows.Forms.ListBox lbIssuedBooks;
+        private System.Windows.Forms.Label lblIssuedBooks;
+        private System.Windows.Forms.Button btnReturnBook;
+        private System.Windows.Forms.ListBox lbReturnedBooks;
+        private System.Windows.Forms.Label lblReturnedBooks;
+        private System.Windows.Forms.Button btnGetAllBooks;
+        private System.Windows.Forms.Button btnGetAllReaders;
+        private System.Windows.Forms.Button btnGetIssuedBooks;
+        private System.Windows.Forms.Button btnGetReturnedBooks;
+    }
+}
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LibraryDesktopApp
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; } = true; // Флаг доступности книги
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Название: {Title}, Автор: {Author}";
+        }
+    }
+
+// Класс читателя
+public class Reader
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public override string ToString()
+    {
+        return $"ID: {Id}, Имя: {Name}";
+    }
+}
+
+// Класс выданных книг
+public class IssuedBook
+{
+    public int Id { get; set; }
+    public Book Book { get; set; }
+    public Reader Reader { get; set; }
+    public DateTime IssueDate { get; set; } = DateTime.Now; // Дата выдачи книги
+    public DateTime ReturnDate { get; set; } // Дата возврата книги (если еще не вернули)
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Книга: {Book.Title}, Читатель: {Reader.Name}, Дата выдачи: {IssueDate.ToShortDateString()}, Дата возврата: {(ReturnDate == default(DateTime) ? "еще не вернули" : ReturnDate.ToShortDateString())}";
+    }
+}
+
+// Класс возвращенных книг
+public class ReturnedBook
+{
+    public int Id { get; set; }
+    public IssuedBook IssuedBook { get; set; }
+    public DateTime ReturnDate { get; set; } = DateTime.Now; // Дата возврата книги
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Книга: {IssuedBook.Book.Title}, Читатель: {IssuedBook.Reader.Name}, Дата возврата: {ReturnDate.ToShortDateString()}";
+    }
+}
+
+// Класс библиотеки
+    public class Library
+    {
+        private List<Book> books = new List<Book>();
+        private List<Reader> readers = new List<Reader>();
+        private List<IssuedBook> issuedBooks = new List<IssuedBook>();
+        private List<ReturnedBook> returnedBooks = new List<ReturnedBook>();
+
+        // Добавление книги
+        public void AddBook(Book book)
+        {
+            book.Id = books.Count + 1;
+            books.Add(book);
+        }
+
+        // Добавление читателя
+        public void AddReader(Reader reader)
+        {
+            reader.Id = readers.Count + 1;
+            readers.Add(reader);
+        }
+
+        // Выдача книги
+        public void IssueBook(Book book, Reader reader)
+        {
+            if (!book.IsAvailable)
+            {
+                Console.WriteLine("Книга уже выдана");
+                return;
+            }
+
+            book.IsAvailable = false;
+
+            var issuedBook = new IssuedBook
+            {
+                Id = issuedBooks.Count + 1,
+                Book = book,
+                Reader = reader
+            };
+
+            issuedBooks.Add(issuedBook);
+
+            Console.WriteLine($"Книга \"{book.Title}\" выдана читателю {reader.Name}");
+        }
+
+        // Возврат книги
+        public void ReturnBook(IssuedBook issuedBook)
+        {
+            if (issuedBook.Book.IsAvailable)
+            {
+                Console.WriteLine("Книга уже в библиотеке");
+                return;
+            }
+
+            issuedBook.Book.IsAvailable = true;
+
+            var returnedBook = new ReturnedBook
+            {
+                Id = returnedBooks.Count + 1,
+                IssuedBook = issuedBook
+            };
+
+            returnedBooks.Add(returnedBook);
+
+            Console.WriteLine($"Книга \"{issuedBook.Book.Title}\" возвращена в библиотеку");
+        }
+
+        // Получение списка всех книг
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        // Получение списка всех читателей
+        public List<Reader> GetAllReaders()
+        {
+            return readers;
+        }
+
+        // Получение списка выданных книг
+        public List<IssuedBook> GetIssuedBooks()
+        {
+            return issuedBooks;
+        }
+
+        // Получение списка возвращенных книг
+        public List<ReturnedBook> GetReturnedBooks()
+        {
+            return returnedBooks;
+        }
+        public string GetReaderNameByBook(Book book)
+        {
+            IssuedBook issuedBook = issuedBooks.FirstOrDefault(ib => ib.Book == book);
+            if (issuedBook != null)
+            {
+                return issuedBook.Reader.Name;
+            }
+            return "Нет данных";
+        }
+    }
+
+    public partial class Form1 : Form
+{
+    private Library library = new Library();
+
+    public Form1()
+    {
+        InitializeComponent();
+        library = new Library();
+    }
+
+    private void btnAddBook_Click(object sender, EventArgs e)
+    {
+        string title = tbBookTitle.Text;
+        string author = tbBookAuthor.Text;
+
+        if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(author))
+        {
+            Book book = new Book { Title = title, Author = author };
+            library.AddBook(book);
+            MessageBox.Show("Книга добавлена");
+
+            // Обновление списка книг
+            UpdateBookList();
+        }
+        else
+        {
+            MessageBox.Show("Пожалуйста, заполните поля названия и автора книги");
+        }
+    }
+
+    private void btnAddReader_Click(object sender, EventArgs e)
+    {
+        string name = tbReaderName.Text;
+
+        if (!string.IsNullOrEmpty(name))
+        {
+            Reader reader = new Reader { Name = name };
+            library.AddReader(reader);
+            MessageBox.Show("Читатель добавлен");
+
+            // Обновление списка читателей
+            UpdateReaderList();
+        }
+        else
+        {
+            MessageBox.Show("Пожалуйста, введите имя читателя");
+        }
+    }
+
+    private void btnIssueBook_Click(object sender, EventArgs e)
+    {
+        if (lbBooks.SelectedItem != null && lbReaders.SelectedItem != null)
+        {
+            Book book = (Book)lbBooks.SelectedItem;
+            Reader reader = (Reader)lbReaders.SelectedItem;
+
+            if (!book.IsAvailable)
+            {
+                MessageBox.Show($"Книга \"{book.Title}\" уже взята читателем {library.GetReaderNameByBook(book)}");
+            }
+            else
+            {
+                library.IssueBook(book, reader);
+                book.IsAvailable = false;
+                MessageBox.Show($"Книга \"{book.Title}\" выдана читателю {reader.Name}");
+
+                // Обновление списка выданных книг
+                UpdateIssuedBookList();
+            }
+        }
+        else
+        {
+            MessageBox.Show("Пожалуйста, выберите книгу и читателя для выдачи");
+        }
+    }
+
+    private void btnReturnBook_Click(object sender, EventArgs e)
+    {
+        if (lbIssuedBooks.SelectedItem != null)
+        {
+            IssuedBook issuedBook = (IssuedBook)lbIssuedBooks.SelectedItem;
+            library.ReturnBook(issuedBook);
+            MessageBox.Show($"Книга \"{issuedBook.Book.Title}\" возвращена в библиотеку");
+
+            // Обновление списка возвращенных книг
+            UpdateReturnedBookList();
+        }
+        else
+        {
+            MessageBox.Show("Пожалуйста, выберите выданную книгу для возврата");
+        }
+    }
+
+    private void btnGetAllBooks_Click(object sender, EventArgs e)
+    {
+        // Обновление списка книг
+        lbBooks.DataSource = null; // Очистка источника данных
+        UpdateBookList();
+    }
+
+    private void btnGetAllReaders_Click(object sender, EventArgs e)
+    {
+        // Обновление списка читателей
+        lbReaders.DataSource = null; // Очистка источника данных
+        UpdateReaderList();
+    }
+
+    private void btnGetIssuedBooks_Click(object sender, EventArgs e)
+    {
+        // Обновление списка выданных книг
+        lbIssuedBooks.DataSource = null; // Очистка источника данных
+        UpdateIssuedBookList();
+    }
+
+    private void btnGetReturnedBooks_Click(object sender, EventArgs e)
+    {
+        // Обновление списка возвращенных книг
+        lbReturnedBooks.DataSource = null; // Очистка источника данных
+        UpdateReturnedBookList();
+    }
+
+    private void UpdateBookList()
+    {
+        List<Book> books = library.GetAllBooks();
+        lbBooks.DataSource = books;
+    }
+
+    private void UpdateReaderList()
+    {
+        List<Reader> readers = library.GetAllReaders();
+        lbReaders.DataSource = readers;
+    }
+
+    private void UpdateIssuedBookList()
+    {
+        List<IssuedBook> issuedBooks = library.GetIssuedBooks();
+
+        if (issuedBooks.Count == 0)
+        {
+            lbIssuedBooks.DataSource = null;
+            MessageBox.Show("Нет выданных книг");
+        }
+        else
+        {
+            lbIssuedBooks.DataSource = issuedBooks;
+        }
+    }
+
+    private void UpdateReturnedBookList()
+    {
+        List<ReturnedBook> returnedBooks = library.GetReturnedBooks();
+        lbReturnedBooks.DataSource = returnedBooks;
+    }
+    
+}
+
+}
